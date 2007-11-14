3c7a611dd02c1a6751c0bcbf64dd3013906a7873
evanweaver <evanweaver@19e92222-5c0b-0410-8929-a290d50e31e9>
2007-11-14 04:44:10 +0000

    close #15616
    git-svn-id: svn+ssh://rubyforge.org/var/svn/mongrel/trunk@884 19e92222-5c0b-0410-8929-a290d50e31e9

--- projects/mongrel_cluster/lib/mongrel_cluster/recipes.rb
+++ projects/mongrel_cluster/lib/mongrel_cluster/recipes.rb
@@ -1,5 +1,5 @@
 
-if respond_to?(:namespace)
+if Capistrano::Configuration.respond_to?(:instance)
   require 'mongrel_cluster/recipes_2' # Cap 2
 else  
   require 'mongrel_cluster/recipes_1' # Cap 1
