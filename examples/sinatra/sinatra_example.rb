debbcdc31db2931b27081661114f023bc99ab266
Eric Wong <normalperson@yhbt.net>
2009-03-09 16:47:19 -0700

    Remove sinatra example
    It is no longer relevant (or working) with rackup compatibility.

--- examples/sinatra/sinatra_example.rb
+++
@@ -1,7 +0,0 @@
-#!/home/ew/bin/ruby
-# this is a standalone Sinatra application, there is absolutely NOTHING
-# special that has to be done in this file for running with Unicorn,
-# instead take a look at the unicorn-sinatra-example script in this
-# directory
-require 'sinatra'
-get('/') { "hello world\n" }
