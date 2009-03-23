018f827ebdc1668d2262ef1337386f896379e0f6
Eric Wong <normalperson@yhbt.net>
2009-03-22 20:52:00 -0700

    benchmark: header values must be strings
    Newer versions of Unicorn "enforce" this by splitting on "\n" to
    handle Rack-style multi-value headers.

--- test/benchmark/response.rb
+++ test/benchmark/response.rb
@@ -14,7 +14,7 @@ count = ENV['count'] ? ENV['count'].to_i : 1
 slice = (' ' * bs).freeze
 body = (1..count).map { slice }.freeze
 hdr = {
-  'Content-Length' => bs * count,
+  'Content-Length' => (bs * count).to_s.freeze,
   'Content-Type' => 'text/plain'.freeze
 }.freeze
 response = [ 200, hdr, body ].freeze
