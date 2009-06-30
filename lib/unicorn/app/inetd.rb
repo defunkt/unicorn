defunkt/unicorn

commit b0013b043a15d77d810d5965157766c1af364db2

Avoid duplicating the "Z" constant
Trying not to repeat ourselves.  Unfortunately, Ruby 1.9 forces
us to actually care about encodings of arbitrary byte sequences.

lib/unicorn/app/inetd.rb
@@ -6,9 +6,6 @@
 module Unicorn::App
   class Inetd
 
-    Z = ''
-    Z.force_encoding(Encoding::BINARY) if Z.respond_to?(:force_encoding)
-
     class CatBody
       def initialize(env, cmd)
         @cmd = cmd

lib/unicorn.rb
@@ -14,6 +14,10 @@ module Unicorn
   autoload :ChunkedReader, 'unicorn/chunked_reader'
   autoload :Util, 'unicorn/util'
 
+  Z = '' # the stock empty string we use everywhere...
+  Z.force_encoding(Encoding::BINARY) if Z.respond_to?(:force_encoding)
+  Z.freeze
+
   class << self
     def run(app, options = {})
       HttpServer.new(app, options).start.join

lib/unicorn/chunked_reader.rb
@@ -6,9 +6,6 @@ module Unicorn; end
 module Unicorn
   class ChunkedReader
 
-    Z = ''
-    Z.force_encoding(Encoding::BINARY) if Z.respond_to?(:force_encoding)
-
     def initialize
       @input = @buf = nil
       @chunk_left = 0

lib/unicorn/http_request.rb
@@ -26,8 +26,6 @@ module Unicorn
       "SERVER_SOFTWARE" => "Unicorn #{Const::UNICORN_VERSION}".freeze
     }
 
-    Z = ''
-    Z.force_encoding(Encoding::BINARY) if Z.respond_to?(:force_encoding)
     NULL_IO = StringIO.new(Z)
     TEE = TeeInput.new
     DECHUNKER = ChunkedReader.new

lib/unicorn/http_response.rb
@@ -31,7 +31,6 @@ module Unicorn
     # Connection: and Date: headers no matter what (if anything) our
     # Rack application sent us.
     SKIP = { 'connection' => true, 'date' => true, 'status' => true }.freeze
-    EMPTY = ''.freeze # :nodoc
     OUT = [] # :nodoc
 
     # writes the rack_response to socket as an HTTP response
@@ -59,7 +58,7 @@ module Unicorn
                    "Date: #{Time.now.httpdate}\r\n" \
                    "Status: #{status}\r\n" \
                    "Connection: close\r\n" \
-                   "#{OUT.join(EMPTY)}\r\n")
+                   "#{OUT.join(Z)}\r\n")
       body.each { |chunk| socket.write(chunk) }
       socket.close # flushes and uncorks the socket immediately
       ensure

lib/unicorn/tee_input.rb
@@ -14,8 +14,6 @@ require 'tempfile'
 
 module Unicorn
   class TeeInput
-    Z = ''
-    Z.force_encoding(Encoding::BINARY) if Z.respond_to?(:force_encoding)
 
     def initialize
       @rd = @wr = @size = @input = nil

commit 1e10654f81e74c4d11ab538b16dcc1b7bd36cb7f

inetd: fix broken constant references
This has been totally broken since
commit b0013b043a15d77d810d5965157766c1af364db2
"Avoid duplicating the "Z" constant"

lib/unicorn/app/inetd.rb
@@ -33,7 +33,7 @@ module Unicorn::App
         # IO.select-able and attempting to make it so would trip Rack::Lint
         @inp_pid = fork {
           [ @err_rd, @out_rd ].each { |io| io.close }
-          buf = Z.dup
+          buf = Unicorn::Z.dup
 
           # this is dependent on @input.read having readpartial semantics:
           while @input.read(16384, buf)
@@ -45,7 +45,7 @@ module Unicorn::App
       end
 
       def each(&block)
-        buf = Z.dup
+        buf = Unicorn::Z.dup
         begin
           rd, = IO.select([@err_rd, @out_rd])
           rd && rd.first or next