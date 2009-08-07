e1d67bef994587ba77e1a3575773755b2b5e1558
Eric Wong <normalperson@yhbt.net>
2009-08-06 22:21:39 -0700

    extconf: SIZEOF_OFF_T should be a ruby.h macro
    Only fallback to check_sizeof() if it is not.  check_sizeof() is
    broken in Ruby 1.9.2preview1 (but fixed in trunk).

--- ext/unicorn_http/extconf.rb
+++ ext/unicorn_http/extconf.rb
@@ -1,6 +1,7 @@
 require 'mkmf'
 
 dir_config("unicorn_http")
-check_sizeof("off_t", "sys/types.h")
+
+have_macro("SIZEOF_OFF_T", "ruby.h") or check_sizeof("off_t", "sys/types.h")
 have_func("rb_str_set_len", "ruby.h")
 create_makefile("unicorn_http")
