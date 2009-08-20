8197825dbeebcaf7da963b75ee8a012e52483f96
Eric Wong <normalperson@yhbt.net>
2009-08-20 02:31:23 -0700

    tee_input: fix rdoc
    the docs for the TeeInput class was clobbering the Unicorn
    module documentation instead.

--- lib/unicorn/tee_input.rb
+++ lib/unicorn/tee_input.rb
@@ -1,16 +1,13 @@
-# Copyright (c) 2009 Eric Wong
-# You can redistribute it and/or modify it under the same terms as Ruby.
-
-# acts like tee(1) on an input input to provide a input-like stream
-# while providing rewindable semantics through a File/StringIO
-# backing store.  On the first pass, the input is only read on demand
-# so your Rack application can use input notification (upload progress
-# and like).  This should fully conform to the Rack::InputWrapper
-# specification on the public API.  This class is intended to be a
-# strict interpretation of Rack::InputWrapper functionality and will
-# not support any deviations from it.
-
 module Unicorn
+
+  # acts like tee(1) on an input input to provide a input-like stream
+  # while providing rewindable semantics through a File/StringIO
+  # backing store.  On the first pass, the input is only read on demand
+  # so your Rack application can use input notification (upload progress
+  # and like).  This should fully conform to the Rack::InputWrapper
+  # specification on the public API.  This class is intended to be a
+  # strict interpretation of Rack::InputWrapper functionality and will
+  # not support any deviations from it.
   class TeeInput < Struct.new(:socket, :req, :parser, :buf)
 
     def initialize(*args)
