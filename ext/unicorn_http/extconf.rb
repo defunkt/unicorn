4f58f4a4d60806af7233193852be134cb74c7c21
Eric Wong <normalperson@yhbt.net>
2009-09-18 16:39:15 -0700

    http: don't force -fPIC if it can't be used
    Not everybody can use it, even if most of the world can.

--- ext/unicorn_http/extconf.rb
+++ ext/unicorn_http/extconf.rb
@@ -1,12 +1,14 @@
 # -*- encoding: binary -*-
 require 'mkmf'
 
-$CFLAGS += " -fPIC " # needed for Rubinius, MRI already uses it regardless
-
 dir_config("unicorn_http")
 
 have_macro("SIZEOF_OFF_T", "ruby.h") or check_sizeof("off_t", "sys/types.h")
 have_macro("SIZEOF_LONG", "ruby.h") or check_sizeof("long", "sys/types.h")
 have_func("rb_str_set_len", "ruby.h")
 have_func("rb_str_modify", "ruby.h")
-create_makefile("unicorn_http")
+
+# -fPIC is needed for Rubinius, MRI already uses it regardless
+with_cflags($CFLAGS + " -fPIC ") do
+  create_makefile("unicorn_http")
+end
