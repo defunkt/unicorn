2243c97edf80d635871bc678794f07d6c1d033c2
Eric Wong <normalperson@yhbt.net>
2010-10-09 00:03:43 +0000

    unicorn 2.0.0pre3 - more small fixes
    There is a new Unicorn::PrereadInput middleware to which allows
    input bodies to be drained off the socket and buffered to disk
    (or memory) before dispatching the application.
    
    HTTP Pipelining behavior is fixed for Rainbows!  There
    are some small Kgio fixes and updates for Rainbows!
    users as well.

--- lib/unicorn/const.rb
+++ lib/unicorn/const.rb
@@ -7,8 +7,8 @@
 # improve things much compared to constants.
 module Unicorn::Const
 
-  # The current version of Unicorn, currently 2.0.0pre2
-  UNICORN_VERSION = "2.0.0pre2"
+  # The current version of Unicorn, currently 2.0.0pre3
+  UNICORN_VERSION = "2.0.0pre3"
 
   # default TCP listen host address (0.0.0.0, all interfaces)
   DEFAULT_HOST = "0.0.0.0"
