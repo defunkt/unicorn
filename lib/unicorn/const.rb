7f3ebe9213e09932cd0e8a2a82bfe2dd5430a824
Eric Wong <normalperson@yhbt.net>
2010-10-27 23:29:55 +0000

    unicorn 1.1.5
    This maintenance release fixes several long-standing but
    recently-noticed bugs.  SIGHUP reloading now correctly restores
    default values if they're erased or commented-out in the Unicorn
    configuration file.  Delays/slowdowns in signal handling since
    0.990 are fixed, too.

--- lib/unicorn/const.rb
+++ lib/unicorn/const.rb
@@ -8,8 +8,8 @@ module Unicorn
   # Symbols did not really improve things much compared to constants.
   module Const
 
-    # The current version of Unicorn, currently 1.1.4
-    UNICORN_VERSION="1.1.4"
+    # The current version of Unicorn, currently 1.1.5
+    UNICORN_VERSION="1.1.5"
 
     DEFAULT_HOST = "0.0.0.0" # default TCP listen host address
     DEFAULT_PORT = 8080      # default TCP listen port
