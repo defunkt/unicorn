2c57f59172c45a3ca52dbddfb3f12c1bc70cbfd6
Eric Wong <normalperson@yhbt.net>
2010-12-29 16:45:13 +0000

    http: remove unnecessary dir_config statement
    We do not link against any external libraries

--- ext/unicorn_http/extconf.rb
+++ ext/unicorn_http/extconf.rb
@@ -1,8 +1,6 @@
 # -*- encoding: binary -*-
 require 'mkmf'
 
-dir_config("unicorn_http")
-
 have_macro("SIZEOF_OFF_T", "ruby.h") or check_sizeof("off_t", "sys/types.h")
 have_macro("SIZEOF_LONG", "ruby.h") or check_sizeof("long", "sys/types.h")
 have_func("rb_str_set_len", "ruby.h")
