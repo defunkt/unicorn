e597e594ad88dc02d70f7d3521d0d3bdc23739bb
Eric Wong <normalperson@yhbt.net>
2011-02-02 16:54:07 -0800

    test_helper: simplify random port binding
    Duh...

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -100,30 +100,17 @@ end
 # for a race condition is very small).  You may also set UNICORN_TEST_ADDR
 # to override the default test address (127.0.0.1).
 def unused_port(addr = '127.0.0.1')
-  retries = 100
-  base = 5000
   port = sock = nil
   begin
-    begin
-      port = base + rand(32768 - base)
-      while port == Unicorn::Const::DEFAULT_PORT
-        port = base + rand(32768 - base)
-      end
-
-      sock = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
-      sock.bind(Socket.pack_sockaddr_in(port, addr))
-      sock.listen(5)
-    rescue Errno::EADDRINUSE, Errno::EACCES
-      sock.close rescue nil
-      retry if (retries -= 1) >= 0
-    end
+    sock = TCPServer.new(addr, 0)
+    port = sock.addr[1]
 
     # since we'll end up closing the random port we just got, there's a race
     # condition could allow the random port we just chose to reselect itself
     # when running tests in parallel with gmake.  Create a lock file while
     # we have the port here to ensure that does not happen .
     lock_path = "#{Dir::tmpdir}/unicorn_test.#{addr}:#{port}.lock"
-    lock = File.open(lock_path, File::WRONLY|File::CREAT|File::EXCL, 0600)
+    File.open(lock_path, File::WRONLY|File::CREAT|File::EXCL, 0600)
     at_exit { File.unlink(lock_path) rescue nil }
   rescue Errno::EEXIST
     sock.close rescue nil
