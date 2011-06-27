fb8bb4469849fa2b2241152aea7e9e82bd3cbcc8
Eric Wong <normalperson@yhbt.net>
2011-06-27 08:12:58 +0000

    unicorn 4.0.0 - for mythical hardware!
    A single Unicorn instance may manage more than 1024 workers
    without needing privileges to modify resource limits.  As a
    result of this, the "raindrops"[1] gem/library is now a required
    dependency.
    
    TCP socket defaults now favor low latency to mimic UNIX domain
    socket behavior (tcp_nodelay: true, tcp_nopush: false).  This
    hurts throughput, users who want to favor throughput should
    specify "tcp_nodelay: false, tcp_nopush: true" in the listen
    directive.
    
    Error logging is more consistent and all lines should be
    formatted correctly in backtraces.  This may break the
    behavior of some log parsers.
    
    The call stack is smaller and thus easier to examine backtraces
    when debugging Rack applications.
    
    There are some internal API changes and cleanups, but none that
    affect applications designed for Rack.  See "git log v3.7.0.."
    for details.
    
    For users who cannot install kgio[2] or raindrops, Unicorn 1.1.x
    remains supported indefinitely.  Unicorn 3.x will remain
    supported if there is demand.  We expect raindrops to introduce
    fewer portability problems than kgio did, however.
    
    [1] http://raindrops.bogomips.org/
    [2] http://bogomips.org/kgio/

--- lib/unicorn/const.rb
+++ lib/unicorn/const.rb
@@ -8,8 +8,8 @@
 # improve things much compared to constants.
 module Unicorn::Const
 
-  # The current version of Unicorn, currently 3.7.0
-  UNICORN_VERSION = "3.7.0"
+  # The current version of Unicorn, currently 4.0.0
+  UNICORN_VERSION = "4.0.0"
 
   # default TCP listen host address (0.0.0.0, all interfaces)
   DEFAULT_HOST = "0.0.0.0"
