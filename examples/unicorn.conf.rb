8478a54008ea64bf734b9dfc78d940ed69bc00ff
Eric Wong <normalperson@yhbt.net>
2012-01-28 09:03:57 +0000

    doc: update doc for Ruby 2.0.0dev CoW-friendliness
    Ruby 2.0.0dev is the future and includes a CoW-friendly GC,
    so we shall encourage folks to give Ruby 2.0.0dev a spin.

--- examples/unicorn.conf.rb
+++ examples/unicorn.conf.rb
@@ -40,7 +40,7 @@ pid "/path/to/app/shared/pids/unicorn.pid"
 stderr_path "/path/to/app/shared/log/unicorn.stderr.log"
 stdout_path "/path/to/app/shared/log/unicorn.stdout.log"
 
-# combine REE with "preload_app true" for memory savings
+# combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) and
