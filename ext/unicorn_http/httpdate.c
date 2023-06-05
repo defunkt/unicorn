defunkt/unicorn

commit a6463151bd1db5b9ae7a54985a200f28f543f241

httpdate: favor gettimeofday(2) over time(2) for correctness
While scanning the git@vger.kernel.org mailing list, I've
learned time(2) may return the wrong value in the first 1 to 2.5
ms of every second.  While I'm not sure if the Date: response
header matters to anyone, returning the correct time seems
prudent.

Link: https://lore.kernel.org/git/20230320230507.3932018-1-gitster@pobox.com/
Link: https://inbox.sourceware.org/libc-alpha/20230306160321.2942372-1-adhemerval.zanella@linaro.org/T/
Link: https://sourceware.org/bugzilla/show_bug.cgi?id=30200

ext/unicorn_http/httpdate.c
@@ -1,5 +1,5 @@
 #include <ruby.h>
-#include <time.h>
+#include <sys/time.h>
 #include <stdio.h>
 
 static const size_t buf_capa = sizeof("Thu, 01 Jan 1970 00:00:00 GMT");
@@ -43,13 +43,24 @@ static struct tm * my_gmtime_r(time_t *now, struct tm *tm)
 static VALUE httpdate(VALUE self)
 {
 	static time_t last;
-	time_t now = time(NULL); /* not a syscall on modern 64-bit systems */
+	struct timeval now;
 	struct tm tm;
 
-	if (last == now)
+	/*
+	 * Favor gettimeofday(2) over time(2), as the latter can return the
+	 * wrong value in the first 1 .. 2.5 ms of every second(!)
+	 *
+	 * https://lore.kernel.org/git/20230320230507.3932018-1-gitster@pobox.com/
+	 * https://inbox.sourceware.org/libc-alpha/20230306160321.2942372-1-adhemerval.zanella@linaro.org/T/
+	 * https://sourceware.org/bugzilla/show_bug.cgi?id=30200
+	 */
+	if (gettimeofday(&now, NULL))
+		rb_sys_fail("gettimeofday");
+
+	if (last == now.tv_sec)
 		return buf;
-	last = now;
-	gmtime_r(&now, &tm);
+	last = now.tv_sec;
+	gmtime_r(&now.tv_sec, &tm);
 
 	/* we can make this thread-safe later if our Ruby loses the GVL */
 	snprintf(buf_ptr, buf_capa,

commit 7e71e740e9ffda995fd6a8f9ecfc3fef6a87b386

httpdate: fix build with Ruby 2.7 (at least)
<time.h> is still required for gmtime_r(3), and not all versions
of <ruby.h> include <time.h>, already.

Fixes: a6463151bd1db5b9 (httpdate: favor gettimeofday(2) over time(2) for correctness, 2023-06-01)

ext/unicorn_http/httpdate.c
@@ -1,4 +1,5 @@
 #include <ruby.h>
+#include <time.h>
 #include <sys/time.h>
 #include <stdio.h>
 