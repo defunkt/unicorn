d71d3285ea29f0978bd0b35a366a1470ca69be18
Eric Wong <bofh@yhbt.net>
2023-09-05 09:14:11 +0000

    update dependency to Ruby 2.5+
    We actually need Ruby 2.3+ for `accept_nonblock(exception: false)';
    and (AFAIK) we can't easily use a subclass of `Socket' while using
    Socket#accept_nonblock to inject WriteSplat support for
    `IO#write(*multi_args)'
    
    So just depend on Ruby 2.5+ since all my Ruby is already on the
    already-ancient Ruby 2.7+ anyways.

--- lib/unicorn/write_splat.rb
+++
@@ -1,7 +0,0 @@
-# -*- encoding: binary -*-
-# compatibility module for Ruby <= 2.4, remove when we go Ruby 2.5+
-module Unicorn::WriteSplat # :nodoc:
-  def write(*arg) # :nodoc:
-    super(arg.join(''))
-  end
-end
